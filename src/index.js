66df5d9cf917e8a51b61cc8772bd9a986bfd615c
Ebenezer <ebenezerdonu@gmail.com>
2019-02-06 08:20:57 +0100

    [Chore: #163348100] add index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import FormContainer from "./components/container/FormContainer.jsx";
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
