45c1cf3b62a0c692d108933dd0294e92695f4d2c
Ebenezer <ebenezerdonu@gmail.com>
2019-02-06 08:45:24 +0100

    [Chore: #163751115] refactor file

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const LandingPage = () => {
-    return (
-        <div>
-            <h1> Store Manager </h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1> Store Manager </h1>
+    </div>
+  )
 }
 
 export default LandingPage
