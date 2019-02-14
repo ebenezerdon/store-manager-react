90fb730a676cb11b82a277c1e4966c3a679ed664
Ebenezer <ebenezerdonu@gmail.com>
2019-02-14 11:55:04 +0100

    [Feature: #163849546] add App.jsx file in src folder

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import thunk from 'redux-thunk';
+import App from './app/App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer autoClose={4000} pauseOnFocusLoss={false} />
+  </Provider>,
+  document.getElementById('app'),
+);
