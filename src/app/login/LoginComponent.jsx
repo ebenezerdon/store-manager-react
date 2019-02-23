19e3764134c7df8d29e935ab3b5a5ad84bc2840c
Ebenezer <ebenezerdonu@gmail.com>
2019-02-23 07:06:45 +0100

    [feature: #163849546] add ellipsis loader

--- src/app/login/LoginComponent.jsx
+++ src/app/login/LoginComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import RingLoaderComponent from '../loaders';
+import { EllipsisLoaderComponent } from '../common/loaders'
 import { constants } from './duck';
-import '../../style/signup.scss';
+import '../style/main.css';
 
 export const LoginComponent = ({ loginUser, loginState }) => {
   const onFormSubmit = e => {
@@ -15,21 +15,25 @@ export const LoginComponent = ({ loginUser, loginState }) => {
     return <Redirect to="/" />;
   }
   return (
-    <div className="login-form" id="login">
-      <form action="" autocomplete="on">
-          <h1>Login Here</h1>
-          <div>
-            <input type="email" placeholder="Email Address" required="" id="emailaddress" autofocus />
+    <main id="login-tag">
+      <div className="container">
+        <div className="login-form" id="login">
+          <form action="" autoComplete="on">
+            <h1>Login Here</h1>
+            <div>
+              <input type="email" placeholder="Email Address" required="" id="emailaddress" autoFocus />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" required="" id="password" />
+            </div>
+            <button type="submit" className="button">Login</button>
+          </form>
+          <div className="ring-loader">
+            {loginState === constants.LOGGING_IN && <EllipsisLoaderComponent />}
           </div>
-          <div>
-            <input type="password" placeholder="Password" required="" id="password" />
-          </div>
-          <button type="submit" class="button">Login</button>
-        </form>
-      <div className="ring-loader">
-        {loginState === constants.LOGGING_IN && <RingLoaderComponent />}
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
