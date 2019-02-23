d2486d9f11f6fbc992c9770a679700e7f674473c
Ebenezer <ebenezerdonu@gmail.com>
2019-02-23 07:41:30 +0100

    [feature: #163849546] add api url

--- src/app/login/duck/operations.js
+++ src/app/login/duck/operations.js
@@ -4,7 +4,7 @@ import actions from './actions';
 import constants from './constants';
 
 const { setLoginState, setLoginError } = actions;
-const url = 'https://learnground-api-staging.herokuapp.com/api/v1/users/login';
+const url = 'https://newstoremanager.herokuapp.com/api/v1/auth/login';
 
 const doLogin = (emailaddress, password) => dispatch => {
   dispatch(setLoginState(constants.LOGGING_IN));
@@ -13,11 +13,15 @@ const doLogin = (emailaddress, password) => dispatch => {
     .post(url, { emailaddress, password })
     .then(({ data }) => {
       localStorage.setItem('accesstoken', data.token);
+      localStorage.setItem('role', data.role);
+      localStorage.setItem('id', data.id);
       dispatch(setLoginState(constants.LOGIN_SUCCESS));
+      console.log(data);
     })
     .catch(({ response }) => {
       dispatch(setLoginState(constants.LOGIN_ERROR));
       dispatch(setLoginError(response.data.error));
+      console.log(response.data);
       toast.error(response.data.error, {
         hideProgressBar: true,
       });
