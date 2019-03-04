0cfd784e2973de1f1ac63213000cd2ec8468cefe
Ebenezer <ebenezerdonu@gmail.com>
2019-03-04 11:47:15 +0100

    [feature: #164186460] add dashboard component to app.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './navbar';
 import { Login } from './login';
+import { Dashboard } from './dashboard';
 
 const App = () => (
   <Router>
@@ -9,6 +10,7 @@ const App = () => (
         <Navbar />
         <Switch>
           <Route path="/" component={Login} exact />
+          <Route path="/dashboard" component={Dashboard} exact />
         </Switch>
       </>
   </Router>
