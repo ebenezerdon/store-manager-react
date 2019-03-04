ed7f01cf1087671f230a81f748a0a81a2445fa9f
Ebenezer <ebenezerdonu@gmail.com>
2019-03-04 11:22:37 +0100

    [chore: #164366140] add reducer for fethAllData

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './app/login';
+import { fetchAllDataReducer } from './app/common';
 
 const rootReducer = combineReducers({
-  login: loginReducer
+  login: loginReducer,
+  fetchAllData: fetchAllDataReducer
 });
 
 export default rootReducer;
