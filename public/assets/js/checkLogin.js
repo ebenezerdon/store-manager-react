dcc08409ca3ad6b9abebdf5d61acbdfc9abd8785
Ebenezer <ebenezerdonu@gmail.com>
2019-03-05 06:54:18 +0100

    [feature: #164392297] remove checklogin.js

--- public/assets/js/checkLogin.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.accesstoken) {
-  if (localStorage.role === 'admin') {
-    window.location.replace('./pages/admindashboard.html');
-  } else {
-    window.location.replace('./pages/attendantdashboard.html');
-  }
-}
