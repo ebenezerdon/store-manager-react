ac418124f1037b1419d60a6f27b222c6fe1df8fe
Ebenezer <ebenezerdonu@gmail.com>
2019-03-05 14:20:29 +0100

    [feature: #164398469] export files

---
+++ src/app/users/duck/index.js
@@ -0,0 +1,5 @@
+export { default as addUserReducer } from './reducers';
+export { default as constants } from './constants';
+export { default as doAddUser } from './operations';
+export { default as actions } from './actions';
+export { default as types } from './types';
