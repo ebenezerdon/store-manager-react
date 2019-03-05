b268553b23c12a57a963c2222f7a3fc304c3eff4
Ebenezer <ebenezerdonu@gmail.com>
2019-03-05 14:19:11 +0100

    [feature: #164398469] add reducer for user signup

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './app/login';
 import { addProductReducer } from './app/products';
+import { addUserReducer } from './app/users';
 import { fetchAllDataReducer } from './app/common';
 
 const rootReducer = combineReducers({
   login: loginReducer,
   fetchAllData: fetchAllDataReducer,
-  addProduct: addProductReducer
+  addProduct: addProductReducer,
+  addUser: addUserReducer
 });
 
 export default rootReducer;
