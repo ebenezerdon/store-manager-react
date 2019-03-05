b4f56efa676cc20856a2629bba349cebc2d28bbc
Ebenezer <ebenezerdonu@gmail.com>
2019-03-05 10:39:28 +0100

    [feature: #164394524] add addProduct reducer to reducers file

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './app/login';
+import { addProductReducer } from './app/products';
 import { fetchAllDataReducer } from './app/common';
 
 const rootReducer = combineReducers({
