b840fba115b86eb489f2b9e3be860d7cd3746997
Ebenezer <ebenezerdonu@gmail.com>
2019-03-06 12:42:44 +0100

    [chore: #164428369] export files

--- src/app/products/duck/index.js
+++ src/app/products/duck/index.js
@@ -1,5 +1,5 @@
 export { default as addProductReducer } from './reducers';
 export { default as constants } from './constants';
-export { default as doAddProduct } from './operations';
+export { doAddProduct, doAddToCart } from './operations';
 export { default as actions } from './actions';
 export { default as types } from './types';
--- src/app/products/index.js
+++ src/app/products/index.js
@@ -1,3 +1,4 @@
 export { ListProductContainer as ListProduct } from './ListProductsContainer';
 export { addProductContainer as AddProduct } from './addProductContainer';
+export { CartContainer as Cart } from './CartContainer';
 export { addProductReducer } from './duck';
