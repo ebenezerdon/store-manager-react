67537f97d9275e46c563f7007978cc29c078cab5
Ebenezer <ebenezerdonu@gmail.com>
2019-03-06 16:23:04 +0100

    [feature: #164428957] add cartContainer file

--- src/app/products/CartContainer.js
+++ src/app/products/CartContainer.js
@@ -1,10 +1,24 @@
 import { connect } from 'react-redux';
+import { doCheckout } from './duck';
 import CartComponent from './CartComponent';
 
 const mapStateToProps = ({
-  fetchAllData: { cart }
-}) => { return { cart }; };
+  fetchAllData: { cart },
+  checkout: { checkoutState, errorMessage }
+}) => { return { cart, checkoutState, errorMessage }; };
 
-const CartContainer = connect(mapStateToProps)(CartComponent);
+const mapDispatchToProps = dispatch => {
+  return {
+    checkout: productDetails =>
+      dispatch(doCheckout(productDetails)),
+    addProduct: productDetails =>
+      dispatch(doAddProduct(productDetails)),
+  };
+};
+
+const CartContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartComponent);
 
 export { CartContainer, mapStateToProps };
