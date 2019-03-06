3261648f89a9406f426ec32ff1592c83e77eb963
Ebenezer <ebenezerdonu@gmail.com>
2019-03-06 16:21:25 +0100

    [feature: #164428957] add doCheckout to operations

--- src/app/products/duck/operations.js
+++ src/app/products/duck/operations.js
@@ -3,7 +3,14 @@ import { toast } from 'react-toastify';
 import actions from './actions';
 import constants from './constants';
 
-const { setAddProductState, setAddProductError, addToCart } = actions;
+const {
+  setAddProductState,
+  setAddProductError,
+  setCheckoutState,
+  setCheckoutError,
+  addToCart,
+  clearCart
+} = actions;
 const apiUrl = process.env.API_URL;
 
 const doAddProduct = productDetails => dispatch => {
@@ -27,9 +34,33 @@ const doAddProduct = productDetails => dispatch => {
     });
 };
 
+const doCheckout = productDetails => dispatch => {
+  dispatch(setCheckoutState(constants.CHECKING_OUT));
+  const headers = {
+    headers: { accesstoken: localStorage.getItem('accesstoken') },
+  };
+  return productDetails.map(product => {
+    axios
+    .post(`${apiUrl}/sales`, product, headers)
+    .then(({ data }) => {
+      dispatch(clearCart(({ cart: [] })));
+      dispatch(setCheckoutState(constants.CHECKOUT_SUCCESS));
+      console.log(data);
+    })
+    .catch(({ response }) => {
+      dispatch(setCheckoutState(constants.CHECKOUT_ERROR));
+      dispatch(setCheckoutError(response.data.error));
+      console.log(response.data);
+      toast.error(response.data.error, {
+        hideProgressBar: true,
+      });
+    });
+  })
+};
+
 const doAddToCart = (cart, product) => dispatch => {
   cart.unshift(product);
   dispatch(addToCart(({ cart })));
 }
 
-export { doAddProduct, doAddToCart };
+export { doAddProduct, doCheckout, doAddToCart };
