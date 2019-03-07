819467f5752afc84471016a0baea94bba47618bb
Ebenezer <ebenezerdonu@gmail.com>
2019-03-08 00:00:55 +0100

    [chore: #164482557] export ellipsis loader

--- src/app/common/index.js
+++ src/app/common/index.js
@@ -1,5 +1,7 @@
 export { fetchAllDataReducer } from './duck';
 export { constants } from './duck';
+export { EllipsisLoaderComponent } from './loaders';
+
 export {
   doFetchCurrentUser,
   doFetchAllUsers,
