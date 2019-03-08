0996c71a81046803009e7064a439370991b91e6e
Ebenezer <ebenezerdonu@gmail.com>
2019-03-08 01:57:41 +0100

    [chore: #164485746] redirect user on logout

--- src/app/navbar/NavbarComponent.jsx
+++ src/app/navbar/NavbarComponent.jsx
@@ -3,35 +3,39 @@ import { Link } from 'react-router-dom';
 import "../style/lib/nav.css"
 
 const NavbarComponent = () => {
-  const role = localStorage.getItem('role');
   const loggedIn = localStorage.getItem('accesstoken');
   const logout = () => {
     localStorage.removeItem('accesstoken');
     localStorage.removeItem('role');
     localStorage.removeItem('userId');
+    window.location.replace('/');
   }
 
   return (
     <div className="navbar-component">
       <div className="top-nav">
-        <Link to="/dashboard">
-          <i className="far fa-user-circle" />
-        </Link>
-        <p>
-          <Link id="name" to="/" onClick={logout}>Logout</Link>
-        </p>
-        <p>
-          <Link id="name" to="/users">Users</Link>
-        </p>
-        <p>
-          <Link id="name" to="/cart">Cart</Link>
-        </p>
-        <p>
-          <Link id="name" to="/sales">Sale Record</Link>
-        </p>
-        <p>
-          <Link id="name" to="/products">Products</Link>
-        </p>
+        {loggedIn &&
+          <>
+            <Link to="/dashboard">
+            <i className="far fa-user-circle" />
+            </Link>
+            <p>
+              <Link id="name" to="/" onClick={logout}>Logout</Link>
+            </p>
+            <p>
+              <Link id="name" to="/users">Users</Link>
+            </p>
+            <p>
+              <Link id="name" to="/cart">Cart</Link>
+            </p>
+            <p>
+              <Link id="name" to="/sales">Sale Record</Link>
+            </p>
+            <p>
+              <Link id="name" to="/products">Products</Link>
+            </p>
+          </>
+        }
       </div>
     </div>
   );
